18f4274c13039167d619d1946c509ce80e996842
Manuel Reale <71377122+manuelreale@users.noreply.github.com>
2020-11-11 11:40:58 +0100

    s

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log('node')
+console.log('node is running');
+
+let express = require('express');
+
+let socket = require('socket.io');
+
+let app = express();
+
+let port= 3001
+
+let server = app.listen(port);
+
+app.use(express.static("public"));
+
+let io = socket(server)
+
+io.on('connection', newConnection);
+
+function newConnection(socket){
+  console.log('new connection'+ socket.client.id)
+}
